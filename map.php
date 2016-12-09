c221d62d82f91347ec8d74f9dad3aaf6d252b16a
jblafon <jb.lechevalier.lafon@gmail.com>
2016-12-09 19:35:30 +0100

    comment control.range

--- map.php
+++ map.php
@@ -6,7 +6,7 @@
 	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<link rel="stylesheet" type="text/css" href="css/leaflet.css" />
-	<link rel="stylesheet" type="text/css" href="css/L.Control.Range.css">
+<!--	<link rel="stylesheet" type="text/css" href="css/L.Control.Range.css">-->
 	<script src="js/L.Control.Range.js"></script>
 	<script src='https://api.mapbox.com/mapbox.js/v2.4.0/mapbox.js'></script>
 
