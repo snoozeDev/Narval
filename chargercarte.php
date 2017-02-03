8e24d34461c7966fdc99e2a256ecf78e5f991055
Jb Lafon <jb.lechevalier.lafon@gmail.com>
2017-02-03 11:05:31 +0100

    Fix Page Load Carte

--- chargercarte.php
+++ chargercarte.php
@@ -3,9 +3,11 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="css/styleIndex.css">
+	<link rel="stylesheet" type="text/css" href="css/chargerCarte.css">
 </head>
 
 
+<!--
 <div id="newDiv" style="color:red">
 			<div class='container'>
 				<div class='loading'>
@@ -19,43 +21,38 @@
 					<span></span>
 				</div>
 			</div>
-		</div>
-		<div id="loadDiv" style="z-index:100">
-			<h4 style="padding-bottom: 5%">Charger une carte sauvegardée</h4>
+</div>
+-->
 
-			<?php
-    include 'db.php';
+<div id="loadDiv" style="z-index:100">
+	<h4 id='loadTitle'>Charger une carte sauvegardée</h4>
+	<a href="index.php">
+		<img id='backIcon' src="image/backArrow.svg" alt="">
+	</a>
 
+<?php
+	include 'db.php';
 
+	$conn = new mysqli($dbhost, $dbuser, $dbpass, $db);
 
-
-
-
-    $conn = new mysqli($dbhost, $dbuser, $dbpass, $db);
-
-    if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-    }
-    $sql = "SELECT id, mapName, mapDescription, cercles, polygs, texts, polyls, batis, bateaux FROM save_marine_table";
-    $result = $conn->query($sql);
-
-
-
-    while($row = $result->fetch_assoc()) {
-
-      echo '
-
-           <ul class="" style="position:relative;z-index:1000">
-      <li class="resource--article ">
-        <h10 class="resource__title" style="color:black">'.$row["mapName"].'</h10>
-        <p class="resource__summary">'.$row["mapDescription"].'</p>
-        <a href="map.php?id='.$row["id"].'" style="cursor: pointer;" class="btn"  id="Load">Charger la carte</a>
-         <a href="delete.php?id='.$row["id"].'" style="background:red;cursor: pointer;" class="btn"  id="Load">Supprimer la carte</a>
+	if ($conn->connect_error) {
+		  die("Connection failed: " . $conn->connect_error);
+	}
+	$sql = "SELECT id, mapName, mapDescription, cercles, polygs, texts, polyls, batis, bateaux FROM save_marine_table";
+	$result = $conn->query($sql);
 
 
-      </ul>
 
-       ';
-
-    }
-    ?>#f5f4f4
+	while($row = $result->fetch_assoc()) {
+		echo '
+		<ul class="" style="position:relative;z-index:1000">
+			<li class="resource--article ">
+				<h10 class="resource__title" style="color:black">'.$row["mapName"].'</h10>
+				<p class="resource__summary">'.$row["mapDescription"].'</p>
+				<a href="map.php?id='.$row["id"].'" style="cursor: pointer;" class="btn"  id="Load">Charger la carte</a>
+				<a href="delete.php?id='.$row["id"].'" style="background:red;cursor: pointer;" class="btn"  id="Load">Supprimer la carte</a>
+			</li>
+		</ul>
+	';
+	}
+?>
