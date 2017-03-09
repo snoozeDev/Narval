8e14b816bcc08cb7f44008f12ac937d10b084a6e
Nicodumontier <nicolas.dumontier@hotmail.com>
2017-03-09 15:23:04 +0100

    Création de la page Crédits (infos.css)

--- infos.php
+++ infos.php
@@ -1,14 +1,30 @@
 <head>
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js" type="text/javascript"></script>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="css/styleIndex.css">
-	<link rel="stylesheet" type="text/css" href="css/chargeEtTuto.css">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js" type="text/javascript"></script>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/styleIndex.css">
+    <link rel="stylesheet" type="text/css" href="css/chargeEtTuto.css">
+    <link rel="stylesheet" type="text/css" href="css/infos.css">
+
 </head>
 
 <body>
-<a href="index.php">
-		<img class="backarrow" src="image/backArrow.svg" alt="">
-		<!--'backIcon' properties in 'changerCarte.css'-->
-	</a>
+    <a href="index.php">
+        <img class="backarrow" src="image/backArrow.svg" alt="">
+        <!--'backIcon' properties in 'changerCarte.css'-->
+    </a>
+
+    <div class="photos">
+
+        <div id="julien"><img src="image/Ju_Langlais.png"></div>
+        <div id="jb"><img src="image/JB_Lafon.png"></div>
+        <div id="nicolas"><img src="image/Nicolas_Dumontier.png"></div>
+        <div id="antoine"><img src="image/Antoine_Delacommune.png"></div>
+        <div id="louis"><img src="image/Louis_Gaudet.png"></div>
+        <div id="baloo"><img src="image/Alexis_Durieu.png"></div>
+        <div id="matthieu"><img src="image/Matthieu_Mignon.png"></div>
+        <div id="alexis"><img src="image/Alexis_Lamarche.png"></div>
+
+    </div>
+
 </body>
